e3976f7517617afefcf303b6fd31aa3def605b79
SandhyaKamurti <96307063+SandhyaKamurti@users.noreply.github.com>
2022-03-01 13:23:54 +0530

    Update anagram.js

--- February/Week4/Assignment5/anagram.js
+++ February/Week4/Assignment5/anagram.js
@@ -6,15 +6,24 @@ anagram(str1, str2);
 function anagram(str1, str2) {
     var strLen1 = str1.length;
     var strLen2 = str2.length;
+    var flag = 0;
     if (strLen1 == strLen2) {
-        var s1 = str1.split('').sort().join('');
-        var s2 = str2.split('').sort().join('');
-        if (s1 == s2) {
-            document.write("Anagram");
-        } else {
-            document.write("Not Anagram");
+        var s1 = str1.split('').sort();
+        var s2 = str1.split('').sort();
+        for (var i = 0; i < strLen1; i++) {
+            for (var j = i; j <= i; j++) {
+                if (s1[i] == s2[j]) {
+                    flag = 1;
+                }
+            }
         }
     } else {
-        document.write("Not Anagram");
+        flag = 0;
+    }
+
+    if (flag == 0) {
+        document.write("Not anagram");
+    } else {
+        document.write("Anagram");
     }
 }
