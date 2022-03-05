cec7a851f552ef2f1874f2baa33f60f66a4a6950
SandhyaKamurti <96307063+SandhyaKamurti@users.noreply.github.com>
2022-03-05 22:27:38 +0530

    Create likeDislike.js

---
+++ March/Week1/Assignment3/likeDislike.js
@@ -0,0 +1,204 @@
+//1
+var c1 = 0;
+
+function like1() {
+    c1 += 1;
+    render1();
+}
+
+function dislike1() {
+    c1 -= 1;
+    render1();
+}
+
+function render1() {
+    var disp = document.getElementById('counter1');
+    disp.innerText = c1;
+}
+//2
+var c2 = 0;
+
+function like2() {
+    c2 += 1;
+    render2();
+}
+
+function dislike2() {
+    c2 -= 1;
+    render2();
+}
+
+function render2() {
+    var disp = document.getElementById('counter2');
+    disp.innerText = c2;
+}
+//3
+var c3 = 0;
+
+function like3() {
+    c3 += 1;
+    render3();
+}
+
+function dislike3() {
+    c3 -= 1;
+    render3();
+}
+
+function render3() {
+    var disp = document.getElementById('counter3');
+    disp.innerText = c3;
+}
+//4
+var c4 = 0;
+
+function like4() {
+    c4 += 1;
+    render4();
+}
+
+function dislike4() {
+    c4 -= 1;
+    render4();
+}
+
+function render4() {
+    var disp = document.getElementById('counter4');
+    disp.innerText = c4;
+}
+//5
+var c5 = 0;
+
+function like5() {
+    c5 += 1;
+    render5();
+}
+
+function dislike5() {
+    c5 -= 1;
+    render5();
+}
+
+function render5() {
+    var disp = document.getElementById('counter5');
+    disp.innerText = c5;
+}
+//6
+var c6 = 0;
+
+function like6() {
+    c6 += 1;
+    render6();
+}
+
+function dislike6() {
+    c6 -= 1;
+    render6();
+}
+
+function render6() {
+    var disp = document.getElementById('counter6');
+    disp.innerText = c6;
+}
+//7
+var c7 = 0;
+
+function like7() {
+    c7 += 1;
+    render7();
+}
+
+function dislike7() {
+    c7 -= 1;
+    render7();
+}
+
+function render7() {
+    var disp = document.getElementById('counter7');
+    disp.innerText = c7;
+}
+//8
+var c8 = 0;
+
+function like8() {
+    c8 += 1;
+    render8();
+}
+
+function dislike8() {
+    c8 -= 1;
+    render8();
+}
+
+function render8() {
+    var disp = document.getElementById('counter8');
+    disp.innerText = c8;
+}
+//9
+var c9 = 0;
+
+function like9() {
+    c9 += 1;
+    render9();
+}
+
+function dislike9() {
+    c9 -= 1;
+    render9();
+}
+
+function render9() {
+    var disp = document.getElementById('counter9');
+    disp.innerText = c9;
+}
+//10
+var c10 = 0;
+
+function like10() {
+    c10 += 1;
+    render10();
+}
+
+function dislike10() {
+    c10 -= 1;
+    render10();
+}
+
+function render10() {
+    var disp = document.getElementById('counter10');
+    disp.innerText = c10;
+}
+//11
+var c11 = 0;
+
+function like11() {
+    c11 += 1;
+    render11();
+}
+
+function dislike11() {
+    c11 -= 1;
+    render11();
+}
+
+function render11() {
+    var disp = document.getElementById('counter11');
+    disp.innerText = c11;
+}
+//12
+var c12 = 0;
+
+function like12() {
+    c12 += 1;
+    render12();
+}
+
+function dislike12() {
+    c12 -= 1;
+    render12();
+}
+
+function render12() {
+    var disp = document.getElementById('counter12');
+    disp.innerText = c12;
+}
