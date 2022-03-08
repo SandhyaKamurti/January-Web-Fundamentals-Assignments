fc4c640fb6cf42b5ab94483d7ebc5546af0d5cb1
SandhyaKamurti <96307063+SandhyaKamurti@users.noreply.github.com>
2022-03-08 15:12:11 +0530

    Update insta.js

--- March/Week2/Assignment1/insta.js
+++ March/Week2/Assignment1/insta.js
@@ -1,4 +1,7 @@
 function myFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
+
+    var popup = document.getElementById("myPopup2");
+    popup.classList.toggle("hide");
 }
