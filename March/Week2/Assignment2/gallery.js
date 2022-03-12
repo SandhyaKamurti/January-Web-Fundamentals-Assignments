81233d118ded13016cb87c59cc1abb9f727db43f
SandhyaKamurti <96307063+SandhyaKamurti@users.noreply.github.com>
2022-03-12 13:04:14 +0530

    Create gallery.js

---
+++ March/Week2/Assignment2/gallery.js
@@ -0,0 +1,16 @@
+ function mainFun(n) {
+            var res = document.getElementById(n);
+            res.style.display = "initial";
+
+            var res2 = document.getElementById('original-image1');
+            res2.style.display = "none";
+        }
+
+
+        function fun2(n) {
+            var res = document.getElementById(n);
+            res.style.display = "none";
+
+            var res2 = document.getElementById('original-image1');
+            res2.style.display = "initial";
+        }
